1e6ac707dd923e547eb0469c07309f5009b2324e
jpsissom <80496269+jpsissom@users.noreply.github.com>
2021-03-11 21:13:26 -0500

    Update Remote Profile Removement.ps1

--- Remote Profile Removement.ps1
+++ Remote Profile Removement.ps1
@@ -4,6 +4,6 @@ $user = Read-Host "Enter User ID: "
 Invoke-Command -ComputerName $computer -ScriptBlock {
     param($user)
     $localpath = 'c:\users\' + $user
-    Get-WmiObject -Class Win32_UserProfile | Where-Object {$_.LocalPath -eq $localpath} | 
+    Get-WmiObject -Class Win32_UserProfile | Where-Object {$_.LocalPath -eq $localpath} |
     Remove-WmiObject
 } -ArgumentList $user
