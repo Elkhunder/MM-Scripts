fe4e302f091742d131fd331d9acfcc7b11c633c1
Elkhunder <80496269+Elkhunder@users.noreply.github.com>
2024-08-16 14:41:05 -0400

    Update Get-WindowsVersion.ps1
    - Added windows version map for windows 10 and 11 build numbers.
    - Added PSCustom Object which is added to an array.  Printing the array to the console displays all the info in a table.
    - Added some error handling to write errors if the computer is unreachable or the os info can't be obtained

--- Get-WindowsVersion.ps1
+++ Get-WindowsVersion.ps1
@@ -1,15 +1,48 @@
+$WinVersionMap = @{
+    '10240' = '1507'    # Windows 10, Version 1507
+    '10586' = '1511'    # Windows 10, Version 1511
+    '14393' = '1607'    # Windows 10, Version 1607 / Windows Server 2016, Version 1607
+    '15063' = '1703'    # Windows 10, Version 1703
+    '16299' = '1709'    # Windows 10, Version 1709
+    '17134' = '1803'    # Windows 10, Version 1803
+    '17763' = '1809'    # Windows 10, Version 1809 / Windows Server 2019, Version 1809
+    '18362' = '1903'    # Windows 10, Version 1903
+    '18363' = '1909'    # Windows 10, Version 1909 / Windows Server, Version 1909
+    '19041' = '2004'    # Windows 10, Version 2004 / Windows Server, Version 2004
+    '19042' = '20H2'    # Windows 10, Version 20H2 / Windows Server, Version 20H2
+    '19043' = '21H1'    # Windows 10, Version 21H1
+    '19044' = '21H2'    # Windows 10, Version 21H2
+    '19045' = '22H2'    # Windows 10, Version 22H2
+    '22000' = '21H2'    # Windows 11, Version 21H2
+    '22621' = '22H2'    # Windows 11, Version 22H2
+    '22631' = '23H2'    # Windows 11, Version 23H2
+}
+
+$DeviceInfoList = @()
 $User = ((Get-CimInstance -ClassName Win32_ComputerSystem).UserName).Split("\")[1]
 $Credential = Get-Credential -UserName "umhs\umhs-$User" -Message "Enter Secondary Credentials"
 $Computers = Read-Host "Enter comma delimited list of computer names"
 
 foreach ($Computer in $Computers.Split(",").Trim()){
     if (Test-Connection -ComputerName $Computer -Count 1 -Quiet){
-        $Session = New-CimSession -Credential $Credential -ComputerName $Computer
-        $WindowsVersion = (Get-CimInstance -CimSession $Session -ClassName Win32_OperatingSystem).Caption
-        Write-Host "$Computer : $WindowsVersion"
+        try {
+            $Session = New-CimSession -Credential $Credential -ComputerName $Computer
+            $Win32_OperatingSystem = Get-CimInstance -CimSession $Session -ClassName Win32_OperatingSystem
+
+            $deviceInfo = [PSCustomObject]@{
+                'Computer' = $Computer
+                'OS Name' = $Win32_OperatingSystem.Caption
+                'OS Version' = $WinVersionMap[$Win32_OperatingSystem.BuildNumber]
+            }
+
+            $deviceInfoList += $deviceInfo
+        }
+        catch {
+            Write-Error "Failed to retrieve OS Information from $Computer. Error: $_"
+        }
     }
     else {
-        Write-Host "$Computer : Offline"
+        Write-Error "$Computer is unreachable"
     }
 }
 Read-Host "Press any key to exit"
